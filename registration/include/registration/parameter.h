43cae09eae5663c7e5c7a7b3ddf04a64efe0ba3e
doggydoggy0101 <dgbshien@gmail.com>
2025-03-05 13:38:07 +0800

    :art: fix default parameter

--- registration/include/registration/parameter.h
+++ registration/include/registration/parameter.h
@@ -8,18 +8,25 @@
 #include <string>
 
 namespace registration {
-
+/**
+ * @param max_iteration The maximum number of iterations allowed.
+ * @param tolerance The tolerance for convergence.
+ * @param robust_type Robust function: Truncated Least Squares (TLS), Geman-McClure (GM) or L0-norm (L0).
+ * @param threshold_c The parameter $c$ of the robust function.
+ * @param gnc_factor Surrogate parameter's update step size.
+ * @param weight_tolerance Stopping critera for weights being binary.
+ * @param majorization Use majorization surrogate for TLS (does nothing for other robust function).
+ * @param superlinear Use superlienar update surrogate parameter for TLS (does nothing for other robust function).
+ */
 struct Params {
-  size_t max_iteration = 100;  // The maximum number of iterations allowed.
-  double tolerance = 1e-6;     // The tolerance for convergence.
-  std::string robust_type =
-      "GM";                  // Robust function: Truncated Least Squares (TLS), Geman-McClure (GM) or L0-norm (L0).
-  double threshold_c = 1.0;  // The parameter $c$ of the robust function.
-  double gnc_factor = 1.4;   // Surrogate parameter's update step size.
-  double weight_tolerance = 1e-4;  // Stopping critera for weights being binary.
-  bool majorization = true;        // Use majorization surrogate for TLS (does nothing for other robust function).
-  bool superlinear =
-      true;  // Use superlienar update surrogate parameter for TLS (does nothing for other robust function).
+  size_t max_iteration = 100; 
+  double tolerance = 1e-6; 
+  std::string robust_type = "GM";
+  double threshold_c = 1.0; 
+  double gnc_factor = 1.4; 
+  double weight_tolerance = 1e-4;
+  bool majorization = false; 
+  bool superlinear = false;
 };
 
 }  // namespace registration
